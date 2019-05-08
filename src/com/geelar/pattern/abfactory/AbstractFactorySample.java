b36f46d93e02f8f23e248953eb7e0167b724469b
geelaro <leegzone@gmail.com>
2019-05-08 23:27:46 +0800

    feat:Add a new PizzaStore

--- src/com/geelar/pattern/abfactory/AbstractFactorySample.java
+++ src/com/geelar/pattern/abfactory/AbstractFactorySample.java
@@ -2,6 +2,8 @@ package com.geelar.pattern.abfactory;
 
 import com.geelar.pattern.abfactory.store.GZPizzaStore;
 import com.geelar.pattern.abfactory.store.PizzaStore;
+import com.geelar.pattern.abfactory.store.XianPizzaStore;
+import com.geelar.pattern.factory2.base.Pizza;
 
 /**
  * 抽象工厂sample
@@ -10,9 +12,14 @@ public class AbstractFactorySample {
 
     public static void main(String[] args) {
 
-        PizzaStore pizzaStore = new GZPizzaStore();
-        pizzaStore.sellPizza("chashao");
-        pizzaStore.sellPizza("cheese");
+        //广州分店
+        PizzaStore gzPizzaStore = new GZPizzaStore();
+        gzPizzaStore.sellPizza("chashao");
+        gzPizzaStore.sellPizza("cheese");
+
+        //西安分店
+        PizzaStore xianStore = new XianPizzaStore();
+        xianStore.sellPizza("lamb"); //特色披萨
 
     }
 }
